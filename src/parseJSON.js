c8d6e86a208aae3186a71a1ab591681d770c5c22
chadam189 <chadam189@gmail.com>
2017-10-09 23:19:33 -0700

    ALMOST pass parseJSON for non-nested arrays

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,49 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  var currentChar = 0;
+  if (json.charAt(currentChar) === '[') {
+    var arr = [];
+    currentChar++;
+    if (json.charAt(currentChar) === ']') {
+      return arr; 
+    } 
+    do {
+    // else -> do while loop? 
+      // determine what type the next value is
+      while (json.charAt(currentChar) === ' ') {
+        currentChar++;
+      }
+        
+      // if string
+      if (json.charAt(currentChar) === '"') {
+        var idx = json.indexOf('"', currentChar + 1);
+        var str = '' + json.slice(currentChar + 1, idx);
+        currentChar = idx + 1;
+        arr.push(str);
+      } else if (json.charAt(currentChar) === 'n') {
+        arr.push(null);
+        currentChar += 4;
+      } else if (json.charAt(currentChar) === 'f') {
+        arr.push(false);
+        currentChar += 5;
+      } else if (json.charAt(currentChar) === 't') {
+        arr.push(true);
+        currentChar += 4;
+      } else {
+      // else if number
+        var ch = json.charAt(currentChar);
+        // ch = parseInt(ch);
+        if (typeof ch === 'number' || json.charAt(currentChar) === '-' || json.charAt(currentChar) === '.') {
+          var numIndex = json.indexOf(',') === -1 ? json.length - 1 : json.indexOf(',');
+          var test = Number(json.slice(currentChar, numIndex));
+          arr.push(test);
+          currentChar = numIndex;
+        }
+      }
+      currentChar++;
+      // what do we look for to determine 
+    } while (json.charAt(currentChar - 1) === ',');
+    return arr;
+  }    
 };
